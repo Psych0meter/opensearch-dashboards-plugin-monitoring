a6d62dc8c19c310092bbf5eec90b84491277d9a4
Psych0meter <38450421+Psych0meter@users.noreply.github.com>
2026-02-12 10:34:00 +0000

    [UPD] Package update for v3.5.0

--- public/plugin.ts
+++ public/plugin.ts
@@ -1,4 +1,10 @@
-import { AppMountParameters, CoreSetup, CoreStart, Plugin, DEFAULT_APP_CATEGORIES } from '../../../src/core/public';
+import { 
+  AppMountParameters, 
+  CoreSetup, 
+  CoreStart, 
+  Plugin, 
+  DEFAULT_APP_CATEGORIES 
+} from '../../../src/core/public';
 import {
   MonitoringPluginSetup,
   MonitoringPluginStart,
@@ -6,24 +12,27 @@ import {
 } from './types';
 import { PLUGIN_ID, PLUGIN_NAME } from '../common';
 
+import { renderApp } from './application'; 
+
 export class MonitoringPlugin implements Plugin<MonitoringPluginSetup, MonitoringPluginStart> {
   public setup(core: CoreSetup): MonitoringPluginSetup {
-    // Register an application into the side navigation menu
+    // Register the application
     core.application.register({
       id: PLUGIN_ID,
       title: PLUGIN_NAME,
       category: DEFAULT_APP_CATEGORIES.management,
       async mount(params: AppMountParameters) {
-        // Load application bundle
-        const { renderApp } = await import('./application');
-        // Get start services as specified in opensearch_dashboards.json
+        // Get start services
         const [coreStart, depsStart] = await core.getStartServices();
-        // Render the application
-        return renderApp(coreStart, depsStart as AppPluginStartDependencies, params);
+        
+        return renderApp(
+          coreStart, 
+          depsStart as AppPluginStartDependencies, 
+          params
+        );
       },
     });
 
-    // Return methods that should be available to other plugins
     return {};
   }
 
